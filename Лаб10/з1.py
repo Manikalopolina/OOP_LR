0c78578eac0b8bf7efac7286b5f08457f88f14e0
Manikalopolina <manikalopolina12@gmail.com>
2023-12-28 10:31:43 +0300

    Лр10

--- Лаб10/з1.py
+++ Лаб10/з1.py
@@ -1,4 +1,4 @@
-class Transport:
+class Transport: # Создание базового класса "Transport"
     def __init__(self, brand, max_speed, kind=None):
         self.brand = brand
         self.max_speed = max_speed
@@ -7,39 +7,41 @@ class Transport:
     def __str__(self):
         return f'Тип транспорта {self.kind} марки {self.brand} может развить скорость {self.max_speed} км/ч'
 
-class Car(Transport):
+
+class Car(Transport): # Создание класса "Car", наследующего от "Transport"
 
     def __init__(self, brand, max_speed, mileage, gasoline_residue):
-        super().__init__(brand,max_speed,kind='Car')
+        super().__init__(brand,max_speed,kind='Car') # Вызов инициализатора родительского класса
         self.mileage = mileage
         self.__gasoline_residue = gasoline_residue
 
     @property
-    def gasoline(self):
+    def gasoline(self): # Геттер для получения остатка бензина
         return f'Осталось бензина на {self.__gasoline_residue} км'
 
     @gasoline.setter
-    def gasoline(self, value):
-        if isinstance(value,int):
-            self.__gasoline_residue += value
+    def gasoline(self, value): # Сеттер для увеличения объема бензина
+        if isinstance(value,int): # Проверка, является ли значение целым числом
+            self.__gasoline_residue += value # Увеличение объема бензина
             print(f'Объем топлива увеличен на {value} л и составляет {self.__gasoline_residue} л')
         else:
             print('Ошибка заправки автомобиля')
 
-class Boat(Transport):
+class Boat(Transport): # Создание  класса "Boat", наследующего от "Transport"
+
 
     def __init__(self, brand, max_speed, owners_name):
-        super().__init__(brand, max_speed, kind='Boat')
+        super().__init__(brand, max_speed, kind='Boat') # Вызов инициализатора родительского класса
         self.owners_name = owners_name
 
     def __str__(self):
         return f'Этой лодкой марки {self.brand} владеет {self.owners_name}'
 
 
-class Plane(Transport):
+class Plane(Transport): # Создание  класса "Plane", наследующего от "Transport"
 
     def __init__(self, brand, max_speed, capacity):
-        super().__init__(brand, max_speed, kind='Plane')
+        super().__init__(brand, max_speed, kind='Plane') # Вызов инициализатора родительского класса
         self.capacity = capacity
 
     def __str__(self):
